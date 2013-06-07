dc7728e66ccdcf93f0a081a3773ac994db89742a
 Lucien Schreiber <lucien.schreiber@gmail.com>
2013-06-07 15:16:18 +0200

    Upating Linux build script

--- src/frame.cpp
+++ src/frame.cpp
@@ -44,7 +44,7 @@ BEGIN_EVENT_TABLE( Frame, wxFrame )
 	EVT_COMMAND( wxID_ANY, vrEVT_TOOL_PAN, Frame::OnToolPanAction)
 	EVT_COMMAND( wxID_ANY, vrEVT_TOOL_SELECT, Frame::OnToolSelectAction)
 	EVT_KEY_DOWN(Frame::OnKeyDown)
-	EVT_KEY_UP(Frame::OnKeyUp)	
+	EVT_KEY_UP(Frame::OnKeyUp)
     EVT_UPDATE_UI(MENU_FRAME_CLEAR_SELECTION, Frame::OnUpdateUIToolClearSelection)
 END_EVENT_TABLE()
 
@@ -61,7 +61,7 @@ bool ctDropFiles::OnDropFiles(wxCoord x, wxCoord y,const wxArrayString & filenam
 	if (filenames.GetCount() == 0) {
 		return false;
 	}
-    
+
 	m_Frame->AddLayers(filenames);
 	return true;
 }
@@ -84,26 +84,26 @@ void Frame::_CreateControls() {
 	bSizer2->Add( m_vrTOC->GetControl(), 1, wxEXPAND, 5 );
 	m_panel1->SetSizer( bSizer2 );
     m_splitter1->Initialize(m_panel1);
-	
+
     wxPanel* m_panel2;
 	m_panel2 = new wxPanel( m_splitter1, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
     wxBoxSizer* bSizer3;
 	bSizer3 = new wxBoxSizer( wxVERTICAL );
-    
+
 	m_vrDisplay = new vrViewerDisplay( m_panel2, wxID_ANY);
 	bSizer3->Add( m_vrDisplay, 1, wxEXPAND, 5 );
 	m_panel2->SetSizer( bSizer3 );
     m_splitter1->Initialize(m_panel2);
-    
+
 	m_splitter1->SplitVertically( m_panel1, m_panel2, 230 );
 	bSizer1->Add( m_splitter1, 1, wxEXPAND, 5 );
-    
+
     m_InfoBar = new WebUpdateInformationBar(this);
     bSizer1->Add(m_InfoBar, wxSizerFlags().Expand());
 
     this->SetSizer(bSizer1);
     bSizer1->SetSizeHints(this);
-    
+
 	m_vrDisplay->SetStatusCoordinates(GetStatusBar(), 0, _("Coordinates not available"));
 	//this->SetSizer( bSizer1 );
 	//this->Layout();
@@ -120,22 +120,22 @@ void Frame::_CreateMenus() {
 #ifndef __WXMAC__
     m_menu1->Append( new wxMenuItem( m_menu1, wxID_EXIT));
 	m_menubar1->Append( m_menu1, _("Project") );
-#endif 
-	
+#endif
+
 	// DATA
 	wxMenu* m_menu5 = new wxMenu();
 	m_menu5->Append( new wxMenuItem( m_menu5, MENU_DATA_ADD,_("Add layer..."), wxEmptyString, wxITEM_NORMAL ) );
 	m_menu5->Append(  new wxMenuItem( m_menu5, MENU_DATA_REMOVE, _("Remove layer...") , wxEmptyString, wxITEM_NORMAL ) );
 	m_menubar1->Append( m_menu5, _("Data") );
-	
+
 	// SELECTION
 	wxMenu* m_menu51;
 	m_menu51 = new wxMenu();
 	m_menu51->Append( new wxMenuItem( m_menu51, MENU_FRAME_SELECT, _("Select tool\tV"), wxEmptyString, wxITEM_NORMAL ));
 	m_menu51->Append( new wxMenuItem( m_menu51, MENU_FRAME_CLEAR_SELECTION,
 									 _("Clear selection"), wxEmptyString, wxITEM_NORMAL ));
-	m_menubar1->Append( m_menu51, _("Selection") ); 
-	
+	m_menubar1->Append( m_menu51, _("Selection") );
+
 	// VIEW
 	wxMenu* m_menu6;
 	m_menu6 = new wxMenu();
@@ -143,13 +143,13 @@ void Frame::_CreateMenus() {
 	m_menu6->Append( new wxMenuItem( m_menu6, MENU_FRAME_PAN, _("Pan tool\tH"), wxEmptyString, wxITEM_NORMAL ));
 	m_menu6->AppendSeparator();
 	m_menu6->Append( new wxMenuItem( m_menu6, wxID_ZOOM_FIT,_("Zoom to Fit\tCtrl+0"), wxEmptyString, wxITEM_NORMAL ) );
-	m_menubar1->Append( m_menu6, _("View") ); 
-	
+	m_menubar1->Append( m_menu6, _("View") );
+
 	// WINDOW
 	wxMenu* m_menu2 = new wxMenu();
 	m_menu2->Append(new wxMenuItem( m_menu2, MENU_WINDOW_LOG, _("Log window...\tCtrl+L"), wxEmptyString, wxITEM_NORMAL ) );
-	
-	// Bug #42, append preference into the Window menu, except for Mac where it goes into Application menu 
+
+	// Bug #42, append preference into the Window menu, except for Mac where it goes into Application menu
 	m_menu2->Append(new wxMenuItem(m_menu1, wxID_PREFERENCES));
 	m_menubar1->Append( m_menu2, _("Window") );
 
@@ -162,7 +162,7 @@ void Frame::_CreateMenus() {
 	m_menu3->Append( new wxMenuItem( m_menu3, MENU_CHECK_UPDATE, _("Check for updates") , wxEmptyString, wxITEM_NORMAL ));
     m_menubar1->Append( m_menu3, _("Help") );
 	this->SetMenuBar( m_menubar1 );
-	
+
 	// ACCELERATORS
 	wxAcceleratorEntry myEntries[3];
 	myEntries[0].Set(wxACCEL_NORMAL, (int) 'V', MENU_FRAME_SELECT);
@@ -203,16 +203,16 @@ void Frame::_CreateAccelerators() {
 	wxAcceleratorEntry myEntry1 (wxACCEL_NORMAL, (int) 'V', MENU_FRAME_SELECT);
 	wxAcceleratorEntry myEntry2 (wxACCEL_NORMAL, (int) 'Z', wxID_ZOOM_IN);
 	wxAcceleratorEntry myEntry3 (wxACCEL_NORMAL, (int) 'H', MENU_FRAME_PAN);
-	
+
 	myAccels.Add(myEntry1);
 	myAccels.Add(myEntry2);
 	myAccels.Add(myEntry3);
-	
+
 	wxAcceleratorEntry * myEntries = new wxAcceleratorEntry[myAccels.GetCount()];
 	for (unsigned int i = 0; i< myAccels.GetCount(); i++) {
 		*(myEntries+i) = myAccels.Item(i);
 	}
-		
+
 	wxAcceleratorTable myAccelTable (myAccels.GetCount(), myEntries);
 	wxASSERT(m_vrDisplay);
 	m_vrDisplay->SetAcceleratorTable(myAccelTable);
@@ -225,7 +225,7 @@ void Frame::_SetSelectedFeature() {
 	if (m_vrViewerLayerManager == NULL) {
 		return;
 	}
-	
+
 	int mySel = m_vrViewerLayerManager->GetSelectionCount();
 	GetStatusBar()->SetStatusText(wxString::Format(_("%d Feature(s) selected"),mySel),1);
 }
@@ -239,10 +239,10 @@ bool Frame::AddLayer(const wxFileName & file, long position, vrRender * render,
 					   file.GetFullName());
 		return false;
 	}
-	
+
 	vrLayer * myLayer = m_vrLayerManager->GetLayer(file);
 	wxASSERT(myLayer);
-	
+
 	// build overviews if needed.
 	wxConfigBase * myConfig = wxConfigBase::Get(false);
 	wxASSERT(myConfig);
@@ -261,7 +261,7 @@ bool Frame::AddLayer(const wxFileName & file, long position, vrRender * render,
 				myFactors.Add(8);
 				myFactors.Add(16);
 				myFactors.Add(32);
-				
+
 				vrProgressSimple myProgressData (this, _("Building Overviews (Pyramids)"),
 												 wxString::Format(_("Building Overviews: '%s'"),
 																  myRaster->GetDisplayName().GetFullName()));
@@ -271,7 +271,7 @@ bool Frame::AddLayer(const wxFileName & file, long position, vrRender * render,
 			}
 		}
 	}
-	
+
 	// add files to the viewer
 	m_vrViewerLayerManager->Add(position, myLayer, render, label, visible);
 	return true;
@@ -286,7 +286,7 @@ bool Frame::_RemoveLayer(long position) {
 		wxLogError(_("Unable to found renderer at position : %ld"),position);
 		return false;
 	}
-	
+
 	vrLayer * myLayer = myRenderer->GetLayer();
 	wxASSERT(myLayer);
 	wxFileName myFile = myRenderer->GetLayer()->GetDisplayName();
@@ -309,7 +309,7 @@ void Frame::_PreferenceChanged(bool refresh) {
 	myConfig->SetPath("..");
 	wxASSERT(m_vrDisplay);
 	m_vrDisplay->SetBackgroundColour(myBackColour);
-	
+
 	// update selection colour
 	vrRender::SetDefaultSelectionColour(mySelColour);
 	if (m_vrViewerLayerManager != NULL) {
@@ -317,19 +317,20 @@ void Frame::_PreferenceChanged(bool refresh) {
 			m_vrViewerLayerManager->GetRenderer(i)->GetRender()->SetSelectionColour(mySelColour);
 		}
 	}
-	
+
 	if (refresh == true) {
 		m_vrViewerLayerManager->Reload();
 	}
-	
+
 }
 
 
 
 void Frame::OnAbout(wxCommandEvent & event) {
-	wxHgVersionDlg myDlg (this, wxID_ANY, _T(""));
-    myDlg.SetBitmapLogo(*_img_app_sml);
-    myDlg.ShowModal();
+	wxHgVersionDlg * myDlg = new wxHgVersionDlg (this, wxID_ANY, _T(""));
+    myDlg->SetBitmapLogo(*_img_app_sml);
+    myDlg->ShowModal();
+    wxDELETE(myDlg);
 }
 
 
@@ -340,7 +341,7 @@ void Frame::OnCheckUpdates(wxCommandEvent & event){
     myConfig->SetPath("INTERNET");
     wxString myProxyInfo = myConfig->Read("PROXY_INFO", wxEmptyString);
     myConfig->SetPath("..");
-        
+
     long mySVNversion = wxNOT_FOUND;
     wxHgVersion::GetSoftNumber().ToLong(&mySVNversion);
     WebUpdateThread * myUpdate = new WebUpdateThread(m_InfoBar, myProxyInfo);
@@ -392,16 +393,16 @@ void Frame::OnLayerAdd(wxCommandEvent & event) {
 							wxEmptyString,
 							myDrivers.GetWildcards(vrDRIVERS_FLAG_ALL),
 							wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_MULTIPLE | wxFD_CHANGE_DIR);
-	
+
 	wxArrayString myPathsFileName;
-	
+
 	if(myFileDlg.ShowModal()==wxID_OK){
 		// try to open files
 		myFileDlg.GetPaths(myPathsFileName);
 		wxASSERT(myPathsFileName.GetCount() > 0);
 		AddLayers(myPathsFileName);
 	}
-	
+
 }
 
 
@@ -430,19 +431,19 @@ void Frame::OnLayerRemove(wxCommandEvent & event) {
 			myDipsIndex = i;
 		}
 	}
-	
+
 	if (myLayersName.IsEmpty()) {
 		wxLogError("No layer opened, nothing to close");
 		return;
 	}
-		
+
 	wxMultiChoiceDialog myChoiceDlg (this, "Select Layer(s) to close",
 									 "Close layer(s)",
 									 myLayersName);
 	if (myChoiceDlg.ShowModal() != wxID_OK) {
 		return;
 	}
-	
+
 	wxArrayInt myLayerToRemoveIndex = myChoiceDlg.GetSelections();
 	if (myLayerToRemoveIndex.IsEmpty()) {
 		wxLogWarning("Nothing selected, no layer will be closed");
@@ -452,14 +453,14 @@ void Frame::OnLayerRemove(wxCommandEvent & event) {
 	// removing layer(s)
 	m_vrViewerLayerManager->FreezeBegin();
 	for (int j = (signed) myLayerToRemoveIndex.GetCount() -1; j >= 0 ; j--) {
-		
+
 		long myIndexToRemove = myLayerToRemoveIndex.Item(j);
 		if (myDipsIndex != wxNOT_FOUND && myDipsIndex <= myIndexToRemove) {
 			myIndexToRemove = myIndexToRemove + 1;
 		}
 		_RemoveLayer(myIndexToRemove);
 	}
-	
+
 	m_vrViewerLayerManager->FreezeEnd();
 }
 
@@ -486,13 +487,13 @@ void Frame::OnKeyDown(wxKeyEvent & event) {
 		event.Skip();
 		return;
 	}
-	
+
 	const vrDisplayTool * myTool = m_vrDisplay->GetTool();
 	if (myTool == NULL) {
 		event.Skip();
 		return;
 	}
-	
+
 	if (myTool->GetID() == wxID_ZOOM_IN) {
 		m_vrDisplay->SetToolZoomOut();
 	}
@@ -506,13 +507,13 @@ void Frame::OnKeyUp(wxKeyEvent & event) {
 		event.Skip();
 		return;
 	}
-	
+
 	const vrDisplayTool * myTool = m_vrDisplay->GetTool();
 	if (myTool == NULL) {
 		event.Skip();
 		return;
 	}
-	
+
 	if (myTool->GetID() == wxID_ZOOM_OUT || myTool->GetID() == wxID_ZOOM_IN) {
 		m_vrDisplay->SetToolZoom();
 	}
@@ -537,16 +538,16 @@ void Frame::OnToolSelect(wxCommandEvent & event) {
 void Frame::OnToolSelectAction(wxCommandEvent & event) {
 	vrDisplayToolMessage * myMsg = (vrDisplayToolMessage*)event.GetClientData();
 	wxASSERT(myMsg);
-	
+
 	m_vrViewerLayerManager->ClearSelection();
 	if(m_vrViewerLayerManager->Select(myMsg->m_Rect) == wxNOT_FOUND){
 		wxDELETE(myMsg);
 		return;
 	}
-	
+
 	m_vrViewerLayerManager->Reload();
 	wxDELETE(myMsg);
-	
+
 }
 
 
@@ -571,16 +572,16 @@ void Frame::OnToolZoomAction(wxCommandEvent & event) {
 	// getting rectangle
 	vrCoordinate * myCoord = m_vrViewerLayerManager->GetDisplay()->GetCoordinate();
 	wxASSERT(myCoord);
-	
+
 	// get real rectangle
 	vrRealRect myRealRect;
 	bool bSuccess = myCoord->ConvertFromPixels(myMsg->m_Rect, myRealRect);
 	wxASSERT(bSuccess == true);
-	
+
 	// get fitted rectangle
 	vrRealRect myFittedRect =myCoord->GetRectFitted(myRealRect);
 	wxASSERT(myFittedRect.IsOk());
-	
+
 	// zoom out
 	if (myMsg->m_EvtType == vrEVT_TOOL_ZOOM) {
 		m_vrViewerLayerManager->Zoom(myFittedRect);
@@ -602,10 +603,10 @@ void Frame::OnToolPan(wxCommandEvent & event) {
 void Frame::OnToolPanAction(wxCommandEvent & event) {
 	vrDisplayToolMessage * myMsg = (vrDisplayToolMessage*)event.GetClientData();
 	wxASSERT(myMsg);
-	
+
 	vrCoordinate * myCoord = m_vrViewerLayerManager->GetDisplay()->GetCoordinate();
 	wxASSERT(myCoord);
-	
+
 	wxPoint myMovedPos = myMsg->m_Position;
 	wxPoint2DDouble myMovedRealPt;
 	if (myCoord->ConvertFromPixels(myMovedPos, myMovedRealPt)==false){
@@ -614,7 +615,7 @@ void Frame::OnToolPanAction(wxCommandEvent & event) {
 		wxDELETE(myMsg);
 		return;
 	}
-	
+
 	vrRealRect myActExtent = myCoord->GetExtent();
 	myActExtent.MoveLeftTopTo(myMovedRealPt);
 	myCoord->SetExtent(myActExtent);
@@ -641,9 +642,9 @@ wxFrame(NULL, FRAME_WINDOW, title) {
 	wxLog * myDlgLog = new tmLogGuiSeverity(wxLOG_Warning);
 	delete wxLog::SetActiveTarget(myDlgLog);
 	m_LogWnd = new wxLogWindow(this, "Fosi Log", false, true);
-    
+
     //setlocale(LC_NUMERIC, "C");
-	
+
 	// preferences
 	wxConfigBase::Set(new wxFileConfig(g_ProgName));
 	m_vrViewerLayerManager = NULL;
@@ -657,7 +658,7 @@ wxFrame(NULL, FRAME_WINDOW, title) {
     // disable help menu into statusbar
     SetStatusBarPane(-1);
     SetStatusText(g_ProgName + _(" version: ") + wxHgVersion::GetSoftNumber(), 2);
-    
+
 	_CreateControls();
 	_CreateMenus();
 	_CreateToolbar();
@@ -670,40 +671,40 @@ wxFrame(NULL, FRAME_WINDOW, title) {
 	// vroomGIS
 	m_vrLayerManager = new vrLayerManager();
 	m_vrViewerLayerManager = new vrViewerLayerManager(m_vrLayerManager, this, m_vrDisplay , m_vrTOC);
-		
+
 	_CreateAccelerators();
-	
+
 	m_vrDisplay->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::OnKeyDown),NULL, this);
 	m_vrDisplay->Connect(wxEVT_KEY_UP, wxKeyEventHandler(Frame::OnKeyUp),NULL, this);
 
 	m_vrDisplay->SetFocus();
 	_PreferenceChanged(false);
-	
+
     // DND
     m_vrTOC->GetControl()->SetDropTarget(new ctDropFiles(this));
-    
+
     wxConfigBase * myConfig =  wxConfigBase::Get(false);
     wxASSERT(myConfig);
     myConfig->SetPath("INTERNET");
     bool bCheckStartup = myConfig->ReadBool("CHECK_AT_STARTUP", true);
     wxString myProxyInfo = myConfig->Read("PROXY_INFO", wxEmptyString);
     myConfig->SetPath("..");
-    
+
     if (bCheckStartup == false) {
         return;
     }
-        
+
     long mySVNversion = wxNOT_FOUND;
     wxHgVersion::GetSoftNumber().ToLong(&mySVNversion);
     WebUpdateThread * myUpdate = new WebUpdateThread(m_InfoBar, myProxyInfo);
-    myUpdate->CheckNewVersion(mySVNversion, true, false, true);    
+    myUpdate->CheckNewVersion(mySVNversion, true, false, true);
 }
 
 
 
 Frame::~Frame() {
 	m_vrDisplay->Disconnect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::OnKeyDown),NULL, this);
-	m_vrDisplay->Disconnect(wxEVT_KEY_UP, wxKeyEventHandler(Frame::OnKeyUp),NULL, this);	
+	m_vrDisplay->Disconnect(wxEVT_KEY_UP, wxKeyEventHandler(Frame::OnKeyUp),NULL, this);
 
 	// don't delete m_vrViewerLayerManager, will be deleted by the manager
 	wxDELETE(m_vrLayerManager);
