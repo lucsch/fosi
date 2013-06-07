366bd06b7e0297ac40f972ee1d38b876e2611f00
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2013-06-07 16:29:29 +0200

    Updating cmake build script

---
+++ build/user-vars-Linux.cmake
@@ -0,0 +1,7 @@
+# BUILD VARIABLES FOR LINUX (UBUNTU HOME)
+
+SET(CMAKE_PREFIX_PATH
+  /home/lucien/programmation/LIB/_LIBGIS
+  /Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBWXSVN #TODO: Change for Linux here 
+)
+# TODO: CHANGE SET(wxWIDGETS_PATH_SVN "/Users/lucien/DATA/PROGRAMATION/_LIB/64/wxWidgets-svn")
